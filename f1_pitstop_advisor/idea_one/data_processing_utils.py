00742d6541de86faac8021a75c879ff284474b9b
kw-00 <christopher.p.wojcik@gmail.com>
2025-09-13 20:53:02 +0200

    Outsource data processing to dedicated util module and prepare training data

--- f1_pitstop_advisor/idea_one/data_processing_utils.py
+++ f1_pitstop_advisor/idea_one/data_processing_utils.py
@@ -28,3 +28,58 @@ def get_lap_data_with_weather(session: Session) -> pd.DataFrame:
 
     data.drop(["TmpJoinIndex", "Time_y"], axis="columns", inplace=True)
     return data
+
+
+def add_lap_time_seconds(data: pd.DataFrame, inplace: bool) -> pd.DataFrame | None:
+    if not inplace:
+        data = data.copy()
+    # Add a column representing lap time in seconds
+    data["LapTimeSeconds"] = data["LapTime"].apply(lambda t: t.total_seconds())
+    if not inplace:
+        return data
+    else:
+        return None
+    
+
+def add_z_score_for_laps(data: pd.DataFrame, inplace: bool) -> pd.DataFrame | None:
+    if not inplace:
+        data = data.copy()
+    if "LapTimeSeconds" not in data.keys():
+        add_lap_time_seconds(data, inplace=True)
+        contained_lap_time_seconds = True
+    else:
+        contained_lap_time_seconds = False
+
+    # Create a DataFrame with drivers as rows and mean/standard deviation of their lap times for columns
+    driver_codes = data["Driver"].unique()
+    driver_df = pd.DataFrame(index=driver_codes)
+    driver_df["MeanTime"] = None
+    driver_df["StandardDeviation"] = None
+
+    for driver in driver_codes:
+        driver_times = data[data["Driver"] == driver]["LapTimeSeconds"]
+        driver_df.loc[driver, "MeanTime"] = driver_times.mean()
+        driver_df.loc[driver, "StandardDeviation"] = driver_times.std()
+        # Calculate Z-score for each lap. This score will tell us how good or bad a lap is 
+        # relative to the rest of the same driver's laps. Lower is better.
+
+    data["LapTimeZScore"] = None
+    average_stdev = driver_df["StandardDeviation"].mean()
+    for idx in data.index:
+        subset = data.loc[idx, ["LapTimeSeconds", "Driver"]]
+        # Lap time for the current lap
+        lap_time = subset["LapTimeSeconds"]
+        driver = subset["Driver"]
+        # Mean lap time for the driver who took the current lap
+        mean_time = driver_df.loc[driver, "MeanTime"]
+        # Add Z-score
+        data.loc[idx, "LapTimeZScore"] = (lap_time - mean_time) / average_stdev
+
+    if not contained_lap_time_seconds:
+        data.drop("LapTimeSeconds", axis="columns", inplace=True)
+    if not inplace:
+        return data
+    else:
+        return None
+    
+    
