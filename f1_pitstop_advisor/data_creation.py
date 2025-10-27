87079b1efb53d80a334d7f51ca5d0c30c38dbaea
kw-00 <christopher.p.wojcik@gmail.com>
2025-10-27 20:07:12 +0100

    Separate data customization from ML-ready data preparation

---
+++ f1_pitstop_advisor/data_creation.py
@@ -0,0 +1,61 @@
+import f1_pitstop_advisor.data_processing_utils as utils
+
+import pandas as pd
+import numpy as np
+import pickle
+
+from fastf1.core import Session
+from typing import Dict, Iterable, List
+
+def _get_refined_lap_data_with_z_score(sessions: List[Session]) -> pd.DataFrame:
+    if not sessions:
+        raise ValueError(f"Parameter \"sessions\" may not be an empty list.")
+    data_list = []
+    for session in sessions:
+        session_data = utils.get_lap_data_with_weather(session)
+        utils.add_z_score_for_laps(session_data, inplace=True)
+        session_data = session_data.convert_dtypes()
+        data_list.append(session_data)
+
+    data = pd.concat(data_list, ignore_index=True)
+
+    # Add a feature determining whether there was a pit stop performed during each lap
+    utils.add_is_pit_lap(data, inplace=True)
+
+    # Select only relevant columns for further processing
+    selected_columns = [
+        "LapTimeZScore",
+        "IsPitLap",
+        "Compound",
+        "TyreLife",
+        "FreshTyre",
+        "LapNumber",
+        "AirTemp",
+        "Humidity",
+        "Pressure",
+        "Rainfall",
+        "TrackTemp",
+        "WindDirection",
+        "WindSpeed"
+    ]
+    filtered_data = data.loc[:, selected_columns]
+
+    # Convert categorical data to boolean values
+    final_data = pd.get_dummies(filtered_data)
+    return final_data
+    
+
+def get_refined_lap_data_with_z_score_by_circuit(sessions: List[Session]) -> Dict[str, pd.DataFrame]:
+    circuits_and_sessions = {}
+    for session in sessions:
+        circuit = session.session_info["Meeting"]["Circuit"]["ShortName"]
+        if circuit not in circuits_and_sessions.keys():
+            circuits_and_sessions[circuit] = []
+        circuits_and_sessions[circuit].append(session)
+
+    dfs = {}
+    for circuit, sessions in circuits_and_sessions.items():
+        dfs[circuit] = _get_refined_lap_data_with_z_score(sessions)
+
+    return dfs
+
--- f1_pitstop_advisor/data_processing_utils.py
+++ f1_pitstop_advisor/data_processing_utils.py
@@ -1,4 +1,4 @@
-from typing import Iterable, List
+from typing import Dict, Iterable, List
 import pandas as pd
 import numpy as np
 import pickle
@@ -18,8 +18,7 @@ def get_lap_data_with_weather(session: Session) -> pd.DataFrame:
     weather_for_intervals = weather_data.loc[:, ["Time"]].copy()
     weather_for_intervals["EndTime"] = weather_for_intervals["Time"].shift(-1)
     weather_for_intervals.loc[weather_for_intervals.last_valid_index(), "EndTime"] = (
-            weather_for_intervals.loc[weather_for_intervals.last_valid_index(), "Time"] + np.timedelta64(1, "m")
-    # type: ignore
+            weather_for_intervals.loc[weather_for_intervals.last_valid_index(), "Time"] + np.timedelta64(1, "m") # type: ignore
     )
     weather_interval_index = pd.IntervalIndex.from_arrays(weather_for_intervals["Time"],
                                                           weather_for_intervals["EndTime"], closed="both")
@@ -34,7 +33,7 @@ def get_lap_data_with_weather(session: Session) -> pd.DataFrame:
     return data
 
 
-def add_lap_time_seconds(data: pd.DataFrame, inplace: bool) -> pd.DataFrame | None:
+def add_lap_time_seconds(data: pd.DataFrame, inplace: bool = True) -> pd.DataFrame | None:
     if not inplace:
         data = data.copy()
     # Add a column representing lap time in seconds
@@ -43,9 +42,19 @@ def add_lap_time_seconds(data: pd.DataFrame, inplace: bool) -> pd.DataFrame | No
         return data
     else:
         return None
+    
+def add_is_pit_lap(data: pd.DataFrame, inplace: bool = True) -> pd.DataFrame | None:
+    if not inplace:
+        data = data.copy()
+
+    data["IsPitLap"] = data["IsPitLap"] = ~np.isnat(data["PitInTime"])
+    if not inplace:
+        return data
+    else:
+        return None
 
 
-def add_z_score_for_laps(data: pd.DataFrame, inplace: bool) -> pd.DataFrame | None:
+def add_z_score_for_laps(data: pd.DataFrame, inplace: bool = True) -> pd.DataFrame | None:
     if not inplace:
         data = data.copy()
     if "LapTimeSeconds" not in data.keys():
@@ -86,59 +95,3 @@ def add_z_score_for_laps(data: pd.DataFrame, inplace: bool) -> pd.DataFrame | No
     else:
         return None
 
-
-def get_refined_lap_data_with_z_score(sessions: List[Session]) -> pd.DataFrame:
-    if not sessions:
-        raise ValueError(f"Parameter \"sessions\" may not be an empty list.")
-    data_list = []
-    for session in sessions:
-        session_data = get_lap_data_with_weather(session)
-        add_z_score_for_laps(session_data, inplace=True)
-        session_data = session_data.convert_dtypes()
-        data_list.append(session_data)
-
-    data = pd.concat(data_list, ignore_index=True)
-
-    # Add a feature determining whether there was a pit stop performed during each lap
-    data["IsPitLap"] = ~np.isnat(data["PitInTime"])
-
-    # Select only relevant columns for further processing
-    selected_columns = [
-        "LapTimeZScore",
-        "IsPitLap",
-        "Compound",
-        "TyreLife",
-        "FreshTyre",
-        "LapNumber",
-        "AirTemp",
-        "Humidity",
-        "Pressure",
-        "Rainfall",
-        "TrackTemp",
-        "WindDirection",
-        "WindSpeed"
-    ]
-    filtered_data = data.loc[:, selected_columns]
-
-    # Convert categorical data to boolean values
-    final_data = pd.get_dummies(filtered_data)
-    return final_data
-
-
-def get_refined_lap_data_with_z_score_for_circuit(sessions: List[Session], circuit: int | str) -> pd.DataFrame:
-    selected_sessions = []
-    for session in sessions:
-        circuit_info = session.session_info["Meeting"]["Circuit"]
-        if isinstance(circuit, int) and circuit == circuit_info["Key"]:
-            session_matches_circuit = True
-        elif isinstance(circuit, str) and circuit == circuit_info["ShortName"]:
-            session_matches_circuit = True
-        else:
-            session_matches_circuit = False
-        if session_matches_circuit:
-            selected_sessions.append(session)
-
-    try:
-        return get_refined_lap_data_with_z_score(selected_sessions)
-    except ValueError:
-        raise KeyError("No sessions found for given circuit.")
