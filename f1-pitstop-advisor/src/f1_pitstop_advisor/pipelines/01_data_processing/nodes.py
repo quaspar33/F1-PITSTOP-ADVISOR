2ed2c5297b7c3e67983946d4f16803dd2b5ee1b8
kw-00 <christopher.p.wojcik@gmail.com>
2025-11-02 19:43:14 +0100

    Implement an example data processing pipeline

--- f1-pitstop-advisor/src/f1_pitstop_advisor/pipelines/01_data_processing/nodes.py
+++ f1-pitstop-advisor/src/f1_pitstop_advisor/pipelines/01_data_processing/nodes.py
@@ -1,12 +1,17 @@
+import datetime
 import pandas as pd
+import pit_advisor.gather_data as gd
+import pit_advisor.data_creation as dc
 
 
 from typing import List
 from fastf1.core import Session
 
 def load_sessions() -> List[Session]:
-    raise NotImplementedError()
+    sessions = gd.get_sessions(datetime.datetime(2025, 10, 20))
+    sessions = [sessions[0]]
+    return gd.load_sessions(sessions)
 
 
 def prepare_data(sessions: List[Session]) -> pd.DataFrame:
-    raise NotImplementedError()
+    return dc._get_refined_lap_data_with_z_score(sessions)
